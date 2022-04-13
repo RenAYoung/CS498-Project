970bdceed2a0ff0db9ec40b924e6cdca437b5893
RenAYoung <ren.a.y.834@gmail.com>
2022-04-13 10:29:42 -0400

    First iteration of item list.

--- item_list.py
+++ item_list.py
@@ -1,12 +1,21 @@
-# file for list of all Items
+# file for list of all items
+
+# import Item class
+import item
 
 # Health Recovery
-
+small_potion = item.Item('Small Potion', 5, 0, 0, 0)
+medium_potion = item.Item('Medium Potion', 10, 0, 0, 0)
+large_potion = item.Item('Large Potion', 15, 0, 0, 0)
 
 # Health Max Increasing
-
+magic_star = item.Item('Magic Star', 0, 10, 0, 0)
+epic_magic_star = item.Item('Epic Magic Star', 0, 25, 0, 0)
 
 # Attack
-
+rusty_sword = item.Item('Rusty Sword', 0, 0, 2, 0)
+basic_sword = item.Item('Basic Sword', 0, 0, 5, 0)
 
 # Defense
+wooden_plank = item.Item('Wooden Plank', 0, 0, 0, 3)
+basic_shield = item.Item('Basic Shield', 0, 0, 0, 6)
