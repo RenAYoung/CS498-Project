28c1d8e387a4b649438d5338efbe5ac22cf1fff5
RenAYoung <ren.a.y.834@gmail.com>
2022-04-15 12:09:09 -0400

    changed how item is imported

--- item_list.py
+++ item_list.py
@@ -1,21 +1,21 @@
 # file for list of all items
 
 # import Item class
-import item
+from item import Item
 
 # Health Recovery
-small_potion = item.Item('Small Potion', 5, 0, 0, 0)
-medium_potion = item.Item('Medium Potion', 10, 0, 0, 0)
-large_potion = item.Item('Large Potion', 15, 0, 0, 0)
+small_potion = Item('Small Potion', 5, 0, 0, 0)
+medium_potion = Item('Medium Potion', 10, 0, 0, 0)
+large_potion = Item('Large Potion', 15, 0, 0, 0)
 
 # Health Max Increasing
-magic_star = item.Item('Magic Star', 0, 10, 0, 0)
-epic_magic_star = item.Item('Epic Magic Star', 0, 25, 0, 0)
+magic_star = Item('Magic Star', 0, 10, 0, 0)
+epic_magic_star = Item('Epic Magic Star', 0, 25, 0, 0)
 
 # Attack
-rusty_sword = item.Item('Rusty Sword', 0, 0, 2, 0)
-basic_sword = item.Item('Basic Sword', 0, 0, 5, 0)
+rusty_sword = Item('Rusty Sword', 0, 0, 2, 0)
+basic_sword = Item('Basic Sword', 0, 0, 5, 0)
 
 # Defense
-wooden_plank = item.Item('Wooden Plank', 0, 0, 0, 3)
-basic_shield = item.Item('Basic Shield', 0, 0, 0, 6)
+wooden_plank = Item('Wooden Plank', 0, 0, 0, 3)
+basic_shield = Item('Basic Shield', 0, 0, 0, 6)
