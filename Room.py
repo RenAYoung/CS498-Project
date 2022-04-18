260bbb00331212dfbc912e2fa3b4f170fb707bc2
Taylor Thomas <stth223@uky.edu>
2022-04-17 22:55:32 -0400

    update Room class

--- Room.py
+++ Room.py
@@ -2,30 +2,43 @@ import math
 
 
 class Room:
-    # static variables (optional)
-    cell_height = 1
-    cell_width = 4
-    grid_size = [15, 19]
 
-    def __init__(self, user_enter=[2, 2], level=None):
-        self.user_location = user_enter  # [x, y]
-        self.level = level
+    def __init__(self, user_entrance=[2, 2], room_id=0, neighbors=[-1, -1, -1, -1]):
+        self.user_location = user_entrance  # current location of user in room [x, y]
+
+        # array of room ids [top, right, bottom, left]
+        self.neighbors = neighbors
+
+        # dimensions for the room
         self.grid_size = [12, 19]  # rows and cols of grid
-        self.cell_width = 4  # width of cells (should be even)
-        self.cell_height = 1  # height of cells (should be odd)
+        self.cell_width = 6  # width of cells (should be even)
+        self.cell_height = 3  # height of cells (should be odd)
+
+        # initialize correct id for the room
+        self.id = room_id
 
     def move_user_position(self, x=0, y=0):
         self.user_location = [self.user_location[0] + x, self.user_location[1] + y]
 
-    def printRoom(self):
+    def print_room(self):
         # print top row cap
         print("_" * (self.grid_size[1] * self.cell_width + 1))
         # print each row
         for i in range(self.grid_size[0]):
-            self.printRow(i)
+            self.print_row(i)
+
+    def has_door(self, which_door):
+        if which_door == 'top':
+            return self.neighbors[0] != -1
+        elif which_door == 'right':
+            return self.neighbors[1] != -1
+        elif which_door == 'bottom':
+            return  self.neighbors[2] != -1
+        elif which_door == 'left':
+            return self.neighbors[3] != -1
 
     # this is the one without the space btwn the rows (bottom cap only)
-    def printRow(self, row_num):
+    def print_row(self, row_num):
         # print cells
         for j in range(self.cell_height):  # print each horizontal cross-section
             for i in range(self.grid_size[1] * self.cell_width):
@@ -52,7 +65,7 @@ class Room:
                     print(" ", end='')
             print("|")  # end cap of each row
 
-    def move_prompt(self):
+    def print_move_prompt(self):
         prompt_string = \
             "(L)eft\n" \
             "(R)ight\n" \
@@ -92,7 +105,7 @@ class Room:
 
 if __name__ == '__main__':
     room = Room()
-    room.printRoom()
+    room.print_room()
     while True:
-        room.move_prompt()
+        room.print_move_prompt()
         room.printRoom()
